4b0541f49a69fe50d4e90261f7dc802d1af823a9
TaeSeong Park <imf4@naver.com>
2019-07-10 22:28:58 +0900

    5-react-graphql 실습 완료

--- starters/client/src/generated/graphql.tsx
+++ starters/client/src/generated/graphql.tsx
@@ -117,3 +117,290 @@ export type ITaskWhereInput = {
 export type ITaskWhereUniqueInput = {
   id?: Maybe<Scalars["ID"]>;
 };
+export type ICreateTaskMutationVariables = {
+  data: ITaskCreateInput;
+};
+
+export type ICreateTaskMutation = { __typename?: "Mutation" } & {
+  createTask: { __typename?: "Task" } & Pick<ITask, "id">;
+};
+
+export type IDeleteTaskMutationVariables = {
+  where: ITaskWhereUniqueInput;
+};
+
+export type IDeleteTaskMutation = { __typename?: "Mutation" } & {
+  deleteTask: Maybe<{ __typename?: "Task" } & Pick<ITask, "id">>;
+};
+
+export type IGetTasksQueryVariables = {};
+
+export type IGetTasksQuery = { __typename?: "Query" } & {
+  tasks: Array<
+    { __typename?: "Task" } & Pick<ITask, "id" | "content" | "isDone">
+  >;
+};
+
+export type IUpdateTaskMutationVariables = {
+  data: ITaskUpdateInput;
+  where: ITaskWhereUniqueInput;
+};
+
+export type IUpdateTaskMutation = { __typename?: "Mutation" } & {
+  updateTask: Maybe<{ __typename?: "Task" } & Pick<ITask, "id">>;
+};
+
+import gql from "graphql-tag";
+import * as React from "react";
+import * as ReactApollo from "react-apollo";
+import * as ReactApolloHooks from "react-apollo-hooks";
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export const CreateTaskDocument = gql`
+  mutation createTask($data: TaskCreateInput!) {
+    createTask(data: $data) {
+      id
+    }
+  }
+`;
+export type ICreateTaskMutationFn = ReactApollo.MutationFn<
+  ICreateTaskMutation,
+  ICreateTaskMutationVariables
+>;
+
+export const CreateTaskComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.MutationProps<
+        ICreateTaskMutation,
+        ICreateTaskMutationVariables
+      >,
+      "mutation"
+    >,
+    "variables"
+  > & { variables?: ICreateTaskMutationVariables }
+) => (
+  <ReactApollo.Mutation<ICreateTaskMutation, ICreateTaskMutationVariables>
+    mutation={CreateTaskDocument}
+    {...props}
+  />
+);
+
+export type ICreateTaskProps<TChildProps = {}> = Partial<
+  ReactApollo.MutateProps<ICreateTaskMutation, ICreateTaskMutationVariables>
+> &
+  TChildProps;
+export function withCreateTask<TProps, TChildProps = {}>(
+  operationOptions?: ReactApollo.OperationOption<
+    TProps,
+    ICreateTaskMutation,
+    ICreateTaskMutationVariables,
+    ICreateTaskProps<TChildProps>
+  >
+) {
+  return ReactApollo.withMutation<
+    TProps,
+    ICreateTaskMutation,
+    ICreateTaskMutationVariables,
+    ICreateTaskProps<TChildProps>
+  >(CreateTaskDocument, {
+    alias: "withCreateTask",
+    ...operationOptions
+  });
+}
+
+export function useCreateTaskMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    ICreateTaskMutation,
+    ICreateTaskMutationVariables
+  >
+) {
+  return ReactApolloHooks.useMutation<
+    ICreateTaskMutation,
+    ICreateTaskMutationVariables
+  >(CreateTaskDocument, baseOptions);
+}
+export const DeleteTaskDocument = gql`
+  mutation deleteTask($where: TaskWhereUniqueInput!) {
+    deleteTask(where: $where) {
+      id
+    }
+  }
+`;
+export type IDeleteTaskMutationFn = ReactApollo.MutationFn<
+  IDeleteTaskMutation,
+  IDeleteTaskMutationVariables
+>;
+
+export const DeleteTaskComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.MutationProps<
+        IDeleteTaskMutation,
+        IDeleteTaskMutationVariables
+      >,
+      "mutation"
+    >,
+    "variables"
+  > & { variables?: IDeleteTaskMutationVariables }
+) => (
+  <ReactApollo.Mutation<IDeleteTaskMutation, IDeleteTaskMutationVariables>
+    mutation={DeleteTaskDocument}
+    {...props}
+  />
+);
+
+export type IDeleteTaskProps<TChildProps = {}> = Partial<
+  ReactApollo.MutateProps<IDeleteTaskMutation, IDeleteTaskMutationVariables>
+> &
+  TChildProps;
+export function withDeleteTask<TProps, TChildProps = {}>(
+  operationOptions?: ReactApollo.OperationOption<
+    TProps,
+    IDeleteTaskMutation,
+    IDeleteTaskMutationVariables,
+    IDeleteTaskProps<TChildProps>
+  >
+) {
+  return ReactApollo.withMutation<
+    TProps,
+    IDeleteTaskMutation,
+    IDeleteTaskMutationVariables,
+    IDeleteTaskProps<TChildProps>
+  >(DeleteTaskDocument, {
+    alias: "withDeleteTask",
+    ...operationOptions
+  });
+}
+
+export function useDeleteTaskMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    IDeleteTaskMutation,
+    IDeleteTaskMutationVariables
+  >
+) {
+  return ReactApolloHooks.useMutation<
+    IDeleteTaskMutation,
+    IDeleteTaskMutationVariables
+  >(DeleteTaskDocument, baseOptions);
+}
+export const GetTasksDocument = gql`
+  query getTasks {
+    tasks {
+      id
+      content
+      isDone
+    }
+  }
+`;
+
+export const GetTasksComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.QueryProps<IGetTasksQuery, IGetTasksQueryVariables>,
+      "query"
+    >,
+    "variables"
+  > & { variables?: IGetTasksQueryVariables }
+) => (
+  <ReactApollo.Query<IGetTasksQuery, IGetTasksQueryVariables>
+    query={GetTasksDocument}
+    {...props}
+  />
+);
+
+export type IGetTasksProps<TChildProps = {}> = Partial<
+  ReactApollo.DataProps<IGetTasksQuery, IGetTasksQueryVariables>
+> &
+  TChildProps;
+export function withGetTasks<TProps, TChildProps = {}>(
+  operationOptions?: ReactApollo.OperationOption<
+    TProps,
+    IGetTasksQuery,
+    IGetTasksQueryVariables,
+    IGetTasksProps<TChildProps>
+  >
+) {
+  return ReactApollo.withQuery<
+    TProps,
+    IGetTasksQuery,
+    IGetTasksQueryVariables,
+    IGetTasksProps<TChildProps>
+  >(GetTasksDocument, {
+    alias: "withGetTasks",
+    ...operationOptions
+  });
+}
+
+export function useGetTasksQuery(
+  baseOptions?: ReactApolloHooks.QueryHookOptions<IGetTasksQueryVariables>
+) {
+  return ReactApolloHooks.useQuery<IGetTasksQuery, IGetTasksQueryVariables>(
+    GetTasksDocument,
+    baseOptions
+  );
+}
+export const UpdateTaskDocument = gql`
+  mutation updateTask($data: TaskUpdateInput!, $where: TaskWhereUniqueInput!) {
+    updateTask(data: $data, where: $where) {
+      id
+    }
+  }
+`;
+export type IUpdateTaskMutationFn = ReactApollo.MutationFn<
+  IUpdateTaskMutation,
+  IUpdateTaskMutationVariables
+>;
+
+export const UpdateTaskComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.MutationProps<
+        IUpdateTaskMutation,
+        IUpdateTaskMutationVariables
+      >,
+      "mutation"
+    >,
+    "variables"
+  > & { variables?: IUpdateTaskMutationVariables }
+) => (
+  <ReactApollo.Mutation<IUpdateTaskMutation, IUpdateTaskMutationVariables>
+    mutation={UpdateTaskDocument}
+    {...props}
+  />
+);
+
+export type IUpdateTaskProps<TChildProps = {}> = Partial<
+  ReactApollo.MutateProps<IUpdateTaskMutation, IUpdateTaskMutationVariables>
+> &
+  TChildProps;
+export function withUpdateTask<TProps, TChildProps = {}>(
+  operationOptions?: ReactApollo.OperationOption<
+    TProps,
+    IUpdateTaskMutation,
+    IUpdateTaskMutationVariables,
+    IUpdateTaskProps<TChildProps>
+  >
+) {
+  return ReactApollo.withMutation<
+    TProps,
+    IUpdateTaskMutation,
+    IUpdateTaskMutationVariables,
+    IUpdateTaskProps<TChildProps>
+  >(UpdateTaskDocument, {
+    alias: "withUpdateTask",
+    ...operationOptions
+  });
+}
+
+export function useUpdateTaskMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    IUpdateTaskMutation,
+    IUpdateTaskMutationVariables
+  >
+) {
+  return ReactApolloHooks.useMutation<
+    IUpdateTaskMutation,
+    IUpdateTaskMutationVariables
+  >(UpdateTaskDocument, baseOptions);
+}
--- starters/client/src/pages/index.tsx
+++ starters/client/src/pages/index.tsx
@@ -1,10 +1,32 @@
 import { useState } from 'react'
+import {
+  IGetTasksQuery,
+  useCreateTaskMutation,
+  useDeleteTaskMutation,
+  useGetTasksQuery,
+  useUpdateTaskMutation,
+} from '~/generated/graphql'
 
 export default function PageIndex() {
   const [content, setContent] = useState('')
 
+  const { error, loading, data, refetch } = useGetTasksQuery()
+  const createTask = useCreateTaskMutation()
+
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setContent(event.target.value)
+  }
+
+  const onAddButtonClick = async () => {
+    await createTask({
+      variables: {
+        data: {
+          content,
+          isDone: false,
+        },
+      },
+    })
+    await refetch()
   }
 
   return (
@@ -12,9 +34,14 @@ export default function PageIndex() {
       <h1>📝 할 일 목록</h1>
       <div className='add'>
         <input type='text' value={content} onChange={onInputChange} />
-        <button>추가하기</button>
+        <button onClick={onAddButtonClick}>추가하기</button>
       </div>
-      <Tasks />
+      <Tasks
+        error={error}
+        loading={loading}
+        data={data}
+        refetch={refetch}
+      />
       <div>
         AWSKRUG 슬랙 가입하기 😎: <a href='https://slack.awskr.org'>https://slack.awskr.org</a>
       </div>
@@ -22,19 +49,68 @@ export default function PageIndex() {
   )
 }
 
-function Tasks() {
+interface ITasksProps {
+  error: any
+  loading: boolean
+  data?: IGetTasksQuery
+  refetch: () => any
+}
+function Tasks(props: ITasksProps) {
+  const updateTask = useUpdateTaskMutation()
+  const deleteTask = useDeleteTaskMutation()
+
+  if (props.error) {
+    return (
+      <div>error</div>
+    )
+  }
+
+  if (props.loading || !props.data || !props.data.tasks) {
+    return (
+      <div>loading...</div>
+    )
+  }
+
   return (
     <ul>
-      <li>
-        <span>id</span>
-        {'\u00A0'}|{'\u00A0'}
-        <span>content</span>
-        {'\u00A0'}
-        <span>✔</span>
-        {'\u00A0'}
-        <button>완료</button>
-        <button>삭제</button>
-      </li>
+      {props.data.tasks.map((task) => {
+        const onCompleteButtonClick = async () => {
+          await updateTask({
+            variables: {
+              data: {
+                isDone: true,
+              },
+              where: {
+                id: task.id,
+              },
+            },
+          })
+          await props.refetch()
+        }
+
+        const onDeleteButtonClick = async () => {
+          await deleteTask({
+            variables: {
+              where: {
+                id: task.id,
+              },
+            },
+          })
+          await props.refetch()
+        }
+
+        return (
+          <li key={task.id}>
+            <span>{task.id}</span>
+            {'\u00A0'}|{'\u00A0'}
+            <span>{task.content}</span>
+            {task.isDone && <span>{'\u00A0'}✔</span>}
+            {'\u00A0'}
+            <button onClick={onCompleteButtonClick}>완료</button>
+            <button onClick={onDeleteButtonClick}>삭제</button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
